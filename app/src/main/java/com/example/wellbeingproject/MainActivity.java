6533c3fc76c0e4ecafb6835626be7d5ac80fdac2
Roberto <robdmc34@gmail.com>
2021-03-08 13:48:46 +0000

    Added in the toolbar xml and added it to MainActivity

--- app/src/main/java/com/example/wellbeingproject/MainActivity.java
+++ app/src/main/java/com/example/wellbeingproject/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.wellbeingproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView backbutton = findViewById(R.id.back_button);
+
+        //to access the back button
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Back button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
