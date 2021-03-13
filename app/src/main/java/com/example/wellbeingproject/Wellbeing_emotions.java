bcd2ea9d8664473d4b06b613f4cbce55cf5e02fc
Roberto <robdmc34@gmail.com>
2021-03-13 17:19:47 +0000

    Added rest of well being pages, emojis, going to set up rest of pages

---
+++ app/src/main/java/com/example/wellbeingproject/Wellbeing_emotions.java
@@ -0,0 +1,29 @@
+package com.example.wellbeingproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Wellbeing_emotions extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wellbeing_emotions);
+
+        ImageView backbutton = findViewById(R.id.back_button);
+
+        //to access the back button
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
