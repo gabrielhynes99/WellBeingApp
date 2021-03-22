9790166a5f40edc614ff91c5f2cda3f886724bde
Gabriel Hynes <c18324463@mytudublin.ie>
2021-03-22 23:20:04 +0000

    Support page added some code

--- app/src/main/java/com/example/wellbeingproject/Support.java
+++ app/src/main/java/com/example/wellbeingproject/Support.java
@@ -1,5 +1,7 @@
 package com.example.wellbeingproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,6 +9,7 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class Support extends AppCompatActivity {
     private Toolbar toolbar;
@@ -25,5 +28,36 @@ public class Support extends AppCompatActivity {
             }
         });
 
+        //References for support buttons
+        CardView emergency = findViewById(R.id.emergency);
+        CardView hse = findViewById(R.id.hse);
+        CardView olympics = findViewById(R.id.olympics);
+
+        //Emergency button
+        emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gov.ie/en/service/89da6-how-to-contact-emergency-services-in-ireland/#"));
+                startActivity(intent);
+            }
+        });
+
+        //HSE button
+        hse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hse.ie/eng/services/list/4/disability/"));
+                startActivity(intent);
+            }
+        });
+
+        //Olympics button
+        olympics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.specialolympics.ie/"));
+                startActivity(intent);
+            }
+        });
     }
 }
