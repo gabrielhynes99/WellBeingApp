aed4c8766032e17139a3235532e02bc9ec8a47af
C18356933 <55754262+C18356933@users.noreply.github.com>
2021-04-14 15:09:31 +0100

    Tts for Tracker page

--- app/src/main/java/com/example/wellbeingproject/Tracker.java
+++ app/src/main/java/com/example/wellbeingproject/Tracker.java
@@ -1,6 +1,7 @@
 package com.example.wellbeingproject;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -8,8 +9,11 @@ import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class Tracker extends AppCompatActivity {
     private Toolbar toolbar;
+    public TextToSpeech ttstracker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,33 @@ public class Tracker extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        final ImageView tts_icon = findViewById(R.id.tts_icon);
+
+        ttstracker = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    ttstracker.setLanguage(Locale.UK);
+
+                    tts_icon.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int i = 0;
+                            while(i == 0) {
+                                ttstracker.speak("How many steps did you take today?", TextToSpeech.QUEUE_ADD, null, null);
+                                ttstracker.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null);
+                                ttstracker.speak("How much water did you drink today?", TextToSpeech.QUEUE_ADD, null, null);
+                                ttstracker.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null);
+                                ttstracker.speak("Track Sleep with Track Sleep Button ", TextToSpeech.QUEUE_ADD, null, null);
+                                ttstracker.playSilentUtterance(1000, TextToSpeech.QUEUE_ADD, null);
+                                i++;
+                            }
+                        }
+                    });
+                }
             }
         });
 
